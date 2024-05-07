7a0b583eb13bb693d68e71d917d4fbbd39794d12
Ilca_Almeida <ilcalmeida92@gmail.com>
2024-05-07 14:55:00 -0300

    feat: Definindo container section faq

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div class="container mx-auto max-w-screen-lg bg-red-400">
+        section faq
+      </div>
+    </>
+  );
+}
+
+export default App;
